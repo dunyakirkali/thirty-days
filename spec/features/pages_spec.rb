decdebbdd3e3c18788c704c611a52e97ceb0c673
Dunya Kirkali <dunyakirkali@gmail.com>
2014-06-22 16:55:46 +0200

    clean up

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -4,22 +4,22 @@ describe 'Pages' do
   
   describe 'welcome page' do
     
-    it 'should show a div' do
+    it 'should show a image with id logoImage' do
       visit root_path
       
-      expect(page).to have_css('#logoImage')
+      expect(page).to have_css('img#logoImage')
     end
     
-    it 'should show a div' do
+    it 'should show a p with class introText' do
       visit root_path
       
-      expect(page).to have_css('.introText')
+      expect(page).to have_css('p.introText')
     end 
     
     it 'should show a sign up button' do
       visit root_path
       
-      expect(page).to have_css('.sign-up-button')
+      expect(page).to have_css('a.sign-up-button')
     end
     
   end
@@ -27,46 +27,44 @@ describe 'Pages' do
   
   describe 'profile page' do
     
-    it 'should show a input' do
-      visit pages_profile_path
+    describe 'user without ongoing plan' do
       
-      expect(page).to have_css('.textField')
-    end
-    
-    it 'should show a submit button' do
-      visit pages_profile_path
+      it 'should show a plan text input' do
+        visit pages_profile_path
       
-      expect(page).to have_css('.submitButton')
-    end
-    
-    it 'should show number of Days' do
-      visit pages_profile_path
+        expect(page).to have_css('.planTextField')
+      end
+            
+      it 'should show a ok button' do
+        visit pages_profile_path
       
-      expect(page).to have_css('.whatText')
-    end
-    
-    it 'should show a plan text input' do
-      visit pages_profile_path
+        expect(page).to have_css('#okButton') 
+      end
       
-      expect(page).to have_css('.planTextField')
-    end
-    it 'should show a ok button' do
-      visit pages_profile_path
+      it 'should show what text' do
+        visit pages_profile_path
       
-      expect(page).to have_css('#okButton') 
-    end
-    
-    it 'should show a saved plan text' do
-      visit pages_profile_path
-      
-      expect(page).to have_css('#savedPlanText')
-    end
-    
-    it 'should show a yes/no buttons' do
-      visit pages_profile_path
-      
-      expect(page).to have_css('.yesnoButtons', count:2)
-    end
-  end
+        expect(page).to have_css('.whatText')
+      end
 
+    end
+    
+    describe 'user with ongoing plan' do
+      it 'should show a saved plan text' do
+        visit pages_profile_path
+      
+        expect(page).to have_css('#savedPlanText')
+      end
+      
+      it 'should show a yes/no buttons' do
+        visit pages_profile_path
+      
+        expect(page).to have_css('.yesnoButtons', count:2)
+      end      
+      
+      it 'should show number of days passed' do
+        pending
+      end
+    end                
+  end
 end
