20b4ff35689a9a099e100785cb17eb9ceaeb6f91
Dunya Kirkali <dunyakirkali@gmail.com>
2014-06-22 16:17:27 +0200

    improved sign up page and shit

--- spec/features/root_path_spec.rb
+++ spec/features/root_path_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 describe 'root' do
   it 'should route signed in user to profile page' do
-    pending
     visit root_path
     
     expect(page).to have_content('Welcome')
   end
   
   it 'should route new user to welcome page' do
+    pending
     visit root_path
     
     expect(page).not_to have_content('Welcome')
