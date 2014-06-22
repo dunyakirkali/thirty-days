1b2ecf9200bbf6cb71b07baffd0fb22fda0b4a8c
Dunya Kirkali <dunyakirkali@gmail.com>
2014-06-22 19:49:51 +0200

    improved user specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ActiveRecord::Base
     !plan.blank? 
   end
   
+  def progress
+    
+  end
+  
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
      data = access_token.info
      user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,18 @@ describe 'User' do
     
     expect(user.has_plan?).to eq(true)
   end
+  
+  describe 'progress' do
+    it 'should be a nil if no plan' do
+      user = FactoryGirl.create(:user, plan: nil)
+      
+      expect(user.progress).to eq(nil)
+    end
+    
+    it 'should be a null array if plan' do
+      user = FactoryGirl.create(:user, plan: Faker::Lorem.paragraph)
+      
+      expect(user.progress.inject{|sum,x| sum + x }).to eq(0)
+    end
+  end
 end
